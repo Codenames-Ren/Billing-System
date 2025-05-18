36182e449176263efed27e34ce68c13f18bf6081
Codenames-Ren <renmizuki38@gmail.com>
2025-05-18 21:45:33 +0700

    Update route api

---
+++ src/controllers/userController.go
@@ -0,0 +1,71 @@
+package controllers
+
+import (
+	"net/http"
+	"ren/backend-api/src/database"
+
+	"ren/backend-api/src/helper"
+	"ren/backend-api/src/models"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func UpdatePassword(c *gin.Context) {
+	var input struct {
+		Oldpassword string `json:"old_password"`
+		Newpassword string `json:"new_password"`
+		Confirmnewpassword string `json:"confirm_new_password"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	//check new password and confirm password match or not
+	if input.Newpassword != input.Confirmnewpassword {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "New password and confirm password doesn't match!"})
+		return
+	}
+
+	//validate new password
+	if valid, message := helper.ValidatePassword(input.Newpassword); !valid {
+		c.JSON(http.StatusBadRequest, gin.H{"error": message})
+		return
+	}
+
+	//get user from JWT token
+	username, exists := c.Get("username")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	//search user by username
+	var user models.User
+	if err := database.DB.Where("username = ?", username).First(&user).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	//checking old password
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Oldpassword)); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Old password is incorrect"})
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Newpassword), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash new password"})
+		return
+	}
+
+	//Updating password in database
+	if err := database.DB.Model(&user).Update("password", string(hashedPassword)).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update password"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Password updated successfuly!"})
+}
---
+++ src/routes/userRoute.go
@@ -0,0 +1,77 @@
+package routes
+
+import (
+	"ren/backend-api/src/controllers"
+	"ren/backend-api/src/middlewares"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func UserRoutes(router *gin.Engine, db *gorm.DB) {
+
+	userGroup := router.Group("/users")
+	{
+		//Endpoint for Register with OTP
+		// userGroup.POST("/register/init", controllers.RegisterInit)
+		// userGroup.POST("/register/verify", controllers.RegisterComplete)
+		// userGroup.POST("/register/resend-otp", controllers.ResendRegisterOTP)
+
+		//Endpoint for login with otp
+		userGroup.POST("/login", controllers.Login)
+		// userGroup.POST("/login/verify", controllers.LoginComplete)
+		// userGroup.POST("/login/resend-otp", controllers.ResendLoginOTP)
+
+		//Endpoint for reset password
+		// userGroup.POST("/forgot-password", controllers.ForgotPassword)
+		// userGroup.POST("/reset-password", controllers.ResetPassword)
+		// userGroup.POST("/forgot-password/resend-otp", controllers.ResendForgotPassOTP)
+		// userGroup.POST("/verify-reset-otp", controllers.VerifyResetOTP)
+
+		//Endpoint where needs auth
+		userGroup.PUT("/update-password", middlewares.AuthMiddleware(), controllers.UpdatePassword)
+		userGroup.GET("/profile", middlewares.AuthMiddleware(), func(c *gin.Context) {
+			username, _ := c.Get("username")
+			c.JSON(200, gin.H{
+				"message": "Welcome to your profile!",
+				"user"	: username,
+			})
+		})
+
+		userGroup.GET("/check-login", middlewares.AuthMiddleware(), func (c *gin.Context) {
+			username, _ := c.Get("username")
+			role, _ := c.Get("role")
+			c.JSON(200, gin.H{
+				"isLoggedIn": true,
+				"username": username,
+				"role": role,
+			})
+		})
+
+		userGroup.POST("/logout", middlewares.AuthMiddleware(), func(c *gin.Context) {
+			c.JSON(200, gin.H{"message": "Logout successful"})
+		})
+	}
+
+	//route group admin
+	adminGroup := router.Group("/admin", middlewares.AuthMiddleware(), middlewares.AdminMiddleware())
+	{
+		adminGroup.GET("/dashboard", func(c *gin.Context) {
+			username, _ := c.Get("username")
+			c.JSON(200, gin.H{
+				"message": "Welcome Admin!",
+				"admin" : username,
+			})
+		})
+
+		//nambah endpoint buat grup admin disini
+		// adminGroup.GET("/users", controllers.GetAllUsers)
+	}
+
+	//superadmin
+	// superAdminGroup := router.Group("/sp-admin", middlewares.AuthMiddleware(), middlewares.SuperAdminMiddleware())
+	// {
+	// 	// superAdminGroup.GET("/users", controllers.GetAllUsers)
+	// 	// superAdminGroup.DELETE("/users/:id", controllers.DeleteUser)
+	// }
+}
